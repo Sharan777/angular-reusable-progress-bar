eb717d29de40ee720d321fc16dfc2743e84c076a
Sharan777 <sharangouda021@gmail.com>
2021-05-30 19:11:23 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, VERSION } from '@angular/core';
-
+import { VERSION } from '@angular/material';
+import { interval, Subscription } from 'rxjs';
+import { OnDestroy } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +9,22 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  progressbarValue = 100;
+  curSec: number = 0;
+
+  startTimer(seconds: number) {
+    const time = seconds;
+    const timer$ = interval(1000);
+
+    const sub = timer$.subscribe((sec) => {
+      this.progressbarValue = 100 - sec * 100 / seconds;
+      this.curSec = sec;
+
+      if (this.curSec === seconds) {
+        sub.unsubscribe();
+      }
+    });
+  }
+
 }
