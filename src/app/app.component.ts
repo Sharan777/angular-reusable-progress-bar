bd31bd59a9ef330b8b3be53bbe64b167390843ce
Sharan777 <sharangouda021@gmail.com>
2021-05-31 19:53:14 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,8 @@ import { OnDestroy } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Reusable Progress bar';
-  uploaded:boolean = false;
-
-  progressbarValue = 5;
-  curSec: number = 0;
-
-  startTimer(seconds: number) {
-    this.uploaded = false;
-    const time = seconds;
-    const timer$ = interval(100);
-
-    const sub = timer$.subscribe(sec => {
-      this.progressbarValue = 10 + (sec * 100) / seconds;
-      this.curSec = sec;
-
-      if (this.curSec === seconds) {
-        this.uploaded = true;
-        sub.unsubscribe();
-      }
-    });
+  childEmit(e) {
+    debugger
+    console.log(e)
   }
 }
--- src/app/shared/upload-progress-bar/upload-progress-bar.component.ts
+++ src/app/shared/upload-progress-bar/upload-progress-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-upload-progress-bar',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-progress-bar.component.css']
 })
 export class UploadProgressBarComponent implements OnInit {
-
-  constructor() { }
+  @Input() parentValue: any;
+  @Output() uploadedEvent = new EventEmitter<string>();
+  progressbarValue: number = 5;
+  curSec: number = 0;
+  constructor() {}
 
   ngOnInit() {
+    this.progressbarValue = this.parentValue;
   }
 
+  name = 'Reusable Progress bar';
+  uploaded: boolean = false;
+
+  startTimer(seconds: number) {
+    this.uploaded = false;
+    const time = seconds;
+    const timer$ = interval(100);
+
+    const sub = timer$.subscribe(sec => {
+      this.progressbarValue = 10 + (sec * 100) / seconds;
+      this.curSec = sec;
+      this.uploadedEvent.emit('STATUS');
+
+      if (this.curSec === seconds) {
+        this.uploaded = true;
+        sub.unsubscribe();
+      }
+    });
+  }
 }
